c2d42f3a0fd50563d828d76562c82f1f535989ff
Ivan Gavalidi <54038039+LaNdErTiS@users.noreply.github.com>
2025-05-01 16:42:50 +0300

    add more validation decor

--- src/praktikum_task/helpers.py
+++ src/praktikum_task/helpers.py
@@ -2,18 +2,23 @@
 Определение различных служебных функций
 """
 
+from collections.abc import Callable
 from functools import wraps
 from typing import Any
 
 from praktikum_task.logger import logger
 
 
-def validate_args(*expected_types: tuple):
+def validate_args(*expected_types) -> Any:
     """
-    Декоратор валидации входящих параметров функций
+    :description: Декоратор валидации входящих параметров функций
+
+    :param *expected_types: Ожидаемые типы аргументов
+
+    :return: Декоратор, который проверяет типы аргументов
     """
 
-    def decorator(func: Any):
+    def decorator(func: Callable) -> Any:
         @wraps(func)
         def wrapper(*args: tuple, **kwargs: dict[str, Any]):
             for arg, expected_type in zip(args, expected_types, strict=True):
@@ -25,3 +30,21 @@ def validate_args(*expected_types: tuple):
         return wrapper
 
     return decorator
+
+
+def validate_no_named_args(func: Callable) -> Any:
+    """
+    :description: Декоратор валидации именованные параметров функций
+
+    :param func: Ожидаемая функция
+
+    :return: Декоратор, который проверяет наличие именованных аргументов
+    """
+
+    @wraps(func)
+    def wrapper(*args: tuple, **kwargs: dict[str, Any]):
+        if kwargs:
+            raise TypeError(f"{func.__name__} не принимает именованные аргументы")
+        return func(*args, **kwargs)
+
+    return wrapper
